1d041922f489948bc94c9198abfc9307b837bb10
Wajid Jawaid <wj241@alumni.cam.ac.uk>
2021-02-02 00:20:02 +0000

    version 3.0

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# enrichR v3.0
+ - Updated enrichR.base.address
+  - Included additional enrichr sites including FlyEnrichr, WormEnrichr, YeastEnrichR and FishEnrichR (Thanks to Alexander Blume)
+ - Ability to generate a plot from the enrichment results (Thanks to I-Hsuan Lin)
+# enrichR v2.1
+ - Fixed bug by adding .onLoad hook to setup initial options
+ - Added bibliography directly and removed .bib as it was causing pandoc-citeproc problems with README.Rmd and vignette.
+ - Corrected spelling error in vignette
+# enrichR v2.0.
+ - Contains checks on enrichR website to ensure it is online.
--- inst/doc/enrichR.R
+++ inst/doc/enrichR.R
@@ -1,36 +1,46 @@
-## ---- echo = FALSE, eval = TRUE------------------------------------------
+## ---- echo = FALSE, eval = TRUE-----------------------------------------------
 websiteLive <- TRUE
 
-## ---- echo = TRUE, eval = FALSE------------------------------------------
+## ---- echo = TRUE, eval = FALSE-----------------------------------------------
 #  library(devtools)
 #  install_github("wjawaid/enrichR")
 
-## ---- echo = TRUE, eval = FALSE------------------------------------------
+## ---- echo = TRUE, eval = FALSE-----------------------------------------------
 #  install.packages("enrichR")
 
-## ---- echo = TRUE, eval = TRUE-------------------------------------------
+## ---- echo = TRUE, eval = TRUE------------------------------------------------
 library(enrichR)
+listEnrichrSites()
+setEnrichrSite("Enrichr") # Human genes
+websiteLive <- TRUE
+
+## ---- echo = TRUE, eval = TRUE------------------------------------------------
 dbs <- listEnrichrDbs()
 
-## ---- echo = TRUE, eval = FALSE------------------------------------------
-#  if (is.null(dbs)) websiteLive <- FALSE
-#  if (websiteLive) head(dbs)
+## ---- echo = TRUE, eval = TRUE------------------------------------------------
+if (is.null(dbs)) websiteLive <- FALSE
+if (websiteLive) head(dbs)
 
-## ---- echo = FALSE, results='asis'---------------------------------------
+## ---- echo = FALSE, results='asis'--------------------------------------------
 library(knitr)
-if (websiteLive) kable(head(dbs[c(1:3,5:6),]))
+if (websiteLive) kable(head(dbs[c(1:6),-4]))
 
-## ---- echo = TRUE, eval = TRUE-------------------------------------------
+## ---- echo = TRUE, eval = TRUE------------------------------------------------
 dbs <- c("GO_Molecular_Function_2015", "GO_Cellular_Component_2015", "GO_Biological_Process_2015")
-if (websiteLive) enriched <- enrichr(c("Runx1", "Gfi1", "Gfi1b", "Spi1", "Gata1", "Kdr"), dbs)
+if (websiteLive) {
+    enriched <- enrichr(c("Runx1", "Gfi1", "Gfi1b", "Spi1", "Gata1", "Kdr"), dbs)
+}
 
-## ---- echo = TRUE, eval = FALSE------------------------------------------
+## ---- echo = TRUE, eval = FALSE-----------------------------------------------
 #  if (websiteLive) enriched[["GO_Biological_Process_2015"]]
 
-## ---- echo = FALSE, results = 'asis'-------------------------------------
+## ---- echo = FALSE, results = 'asis'------------------------------------------
 if (websiteLive) {
  x <- head(enriched[["GO_Biological_Process_2015"]])
  x[,1] <- gsub("GO:", "GO_", x[,1])
  kable(x)
 }
+
+## ---- echo = TRUE, eval = TRUE, fig.width = 8, fig.height = 6, fig.align = "center", dpi = 100----
+if (websiteLive) plotEnrich(enriched[[3]], showTerms = 20, numChar = 40, y = "Count", orderBy = "P.value")
 
