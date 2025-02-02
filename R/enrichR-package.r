7ddde0d6991bb38240f97f297f1388547101389a
Wajid Jawaid <wajid.jawaid@gmail.com>
2025-02-02 22:50:06 +0000

    version 3.4

--- R/enrichR-package.r
+++ R/enrichR-package.r
@@ -3,10 +3,10 @@
 #' The enrichR package provides an R interface to all enrichR (https://maayanlab.cloud/Enrichr/) databases.
 #'
 #' @name enrichR-package
-#' @author Wajid Jawaid \email{wj241@alumni.cam.ac.uk}
-#' @docType package
-#' @keywords package
-NULL
+#' @author Wajid Jawaid \email{wajid.jawaid@gmail.com}
+#' @keywords internal
+"_PACKAGE"
+
 
 #' 790 gene symbols
 #'
@@ -14,9 +14,38 @@ NULL
 #'
 #' @name genes790
 #' @docType data
+#' @usage data(genes790)
 #' @format vector
 #' @keywords datasets
 #' @examples
 #' data(genes790)
 #' length(genes790)
+NULL
+
+#' Example input genes
+#'
+#' This is a character vector which consists of 375 gene symbols taken from the Enrichr website.
+#'
+#' @name input
+#' @docType data
+#' @usage data(input)
+#' @format vector
+#' @keywords datasets
+#' @examples
+#' data(input)
+#' length(input)
+NULL
+
+#' Example background genes
+#'
+#' This is a character vector which consists of 20,625 gene symbols taken from the Enrichr website.
+#'
+#' @name background
+#' @docType data
+#' @usage data(background)
+#' @format vector
+#' @keywords datasets
+#' @examples
+#' data(background)
+#' length(background)
 NULL
--- inst/doc/enrichR.R
+++ inst/doc/enrichR.R
@@ -1,53 +1,108 @@
-## ---- echo = FALSE, eval = TRUE-----------------------------------------------
+## ----echo = FALSE, eval = TRUE------------------------------------------------
 websiteLive <- TRUE
 
-## ---- echo = TRUE, eval = FALSE-----------------------------------------------
-#  library(devtools)
-#  install_github("wjawaid/enrichR")
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+# library(devtools)
+# install_github("wjawaid/enrichR")
 
-## ---- echo = TRUE, eval = FALSE-----------------------------------------------
-#  install.packages("enrichR")
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+# install.packages("enrichR")
 
-## ---- echo = TRUE, eval = TRUE------------------------------------------------
+## ----echo = TRUE, eval = TRUE-------------------------------------------------
 library(enrichR)
+
 websiteLive <- getOption("enrichR.live")
+
 if (websiteLive) {
     listEnrichrSites()
-    setEnrichrSite("Enrichr") # Human genes   
+    setEnrichrSite("Enrichr") # Human genes
 }
 
-## ---- echo = TRUE, eval = TRUE------------------------------------------------
-if (websiteLive) dbs <- listEnrichrDbs()
-
-## ---- echo = TRUE, eval = TRUE------------------------------------------------
-## if (is.null(dbs)) websiteLive <- FALSE
-if (websiteLive) head(dbs)
-
-## ---- echo = FALSE, results='asis'--------------------------------------------
-library(knitr)
-if (websiteLive) kable(head(dbs[c(1:6),-4]))
-
-## ---- echo = TRUE, eval = TRUE------------------------------------------------
-dbs <- c("GO_Molecular_Function_2015", "GO_Cellular_Component_2015", "GO_Biological_Process_2015")
+## ----echo = TRUE, eval = TRUE-------------------------------------------------
 if (websiteLive) {
-    enriched <- enrichr(c("Runx1", "Gfi1", "Gfi1b", "Spi1", "Gata1", "Kdr"), dbs)
+    dbs <- listEnrichrDbs()
+    head(dbs)
 }
 
-## ---- echo = TRUE, eval = FALSE-----------------------------------------------
-#  if (websiteLive) enriched[["GO_Biological_Process_2015"]]
+## ----echo = TRUE, eval = TRUE-------------------------------------------------
+dbs <- c("GO_Molecular_Function_2023", "GO_Cellular_Component_2023", 
+	 "GO_Biological_Process_2023")
 
-## ---- echo = FALSE, results = 'asis'------------------------------------------
-success <- ifelse(exists("enriched"), TRUE, FALSE) & websiteLive
-success <- success & (length(enriched) >= 3)
-success <- success & all(dim(enriched[[3]]) > 2)                  
+## ----echo = TRUE, eval = TRUE-------------------------------------------------
+# Load example input genes
+data(input)
+length(input)
+head(input)
+
+if (websiteLive) {
+    enriched <- enrichr(input, dbs)
+}
+
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+# if (websiteLive) head(enriched[["GO_Biological_Process_2023"]])
+
+## ----echo = FALSE, results = 'asis'-------------------------------------------
+success <- websiteLive & (length(enriched) >= 3)
+success <- success & all(dim(enriched[["GO_Biological_Process_2023"]]) > 2)
 if (success) {
-    x <- head(enriched[["GO_Biological_Process_2015"]])
-    x[,1] <- gsub("GO:", "GO_", x[,1])
-    kable(x)
+    x <- head(enriched[["GO_Biological_Process_2023"]])
+    x[,1] <- gsub(":", "&#58;", x[,1])
+    knitr::kable(x)
 }
 
-## ---- echo = TRUE, eval = TRUE, fig.width = 8, fig.height = 6, fig.align = "center", dpi = 100----
+## ----echo = TRUE, eval = TRUE-------------------------------------------------
+# Load example background
+data(background)
+length(background)
+head(background)
+
 if (websiteLive) {
-    plotEnrich(enriched[[3]], showTerms = 20, numChar = 40, y = "Count", orderBy = "P.value")
+    enriched2 <- enrichr(input, dbs, background = background)
 }
+
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+# if (websiteLive) head(enriched2[["GO_Biological_Process_2023"]])
+
+## ----echo = FALSE, results = 'asis'-------------------------------------------
+success <- websiteLive & (length(enriched2) >= 3)
+success <- success & all(dim(enriched2[["GO_Biological_Process_2023"]]) > 2)
+if (success) {
+    x <- head(enriched2[["GO_Biological_Process_2023"]])
+    x[,1] <- gsub(":", "&#58;", x[,1])
+    knitr::kable(x)
+}
+
+## ----echo = TRUE, eval = TRUE-------------------------------------------------
+if (websiteLive) {
+    # Replace 'Rank' with 'Overlap' column, calculated with GMT files
+    enriched3 <- enrichr(input, dbs, background = background, include_overlap = TRUE)
+}
+
+## ----echo = FALSE, results = 'asis'-------------------------------------------
+success <- (length(enriched3) >= 3) & all(dim(enriched3[["GO_Biological_Process_2023"]]) > 2)
+if (success) {
+    x <- head(enriched3[["GO_Biological_Process_2023"]])
+    x[,1] <- gsub(":", "&#58;", x[,1])
+    knitr::kable(x)
+}
+
+## ----echo = TRUE, eval = TRUE, fig.width = 8, fig.height = 5, fig.align = "center", dpi = 100----
+if (websiteLive) {
+    plotEnrich(enriched[["GO_Biological_Process_2023"]], showTerms = 20, numChar = 40, 
+	       y = "Count", orderBy = "P.value")
+}
+
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+# # To text files
+# printEnrich(enriched)
+# 
+# # To Excel
+# printEnrich(enriched, outFile = "excel")
+
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+# options(RCurlOptions = list(proxy = 'http://ip_or_url',
+#                             proxyusername = 'myuser',
+#                             proxypassword = 'mypwd',
+#                             proxyport = 'port_num',
+#                             proxyauth = 'basic'))
 
